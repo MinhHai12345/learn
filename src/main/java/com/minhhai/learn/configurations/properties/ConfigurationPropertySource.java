74c04729f179c7732b923fca9a2382fd835e02b9
Hai Vo <hai.minh@digicommercegroup.com>
2022-06-22 11:19:35 +0700

    check commit

--- src/main/java/com/minhhai/learn/configurations/properties/ConfigurationPropertySource.java
+++ src/main/java/com/minhhai/learn/configurations/properties/ConfigurationPropertySource.java
@@ -1,30 +1,30 @@
-//package com.minhhai.learn.configurations.properties;
-//
-//import com.minhhai.learn.entities.ConfigEntity;
-//import com.minhhai.learn.repository.ConfigRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.EnvironmentAware;
-//import org.springframework.core.env.ConfigurableEnvironment;
-//import org.springframework.core.env.Environment;
-//import org.springframework.core.env.MapPropertySource;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//@Component("configurationPropertySource")
-//public class ConfigurationPropertySource implements EnvironmentAware {
-//
-//    @Autowired
-//    private ConfigRepository configRepository;
-//
-//    @Override
-//    public void setEnvironment(Environment environment) {
-//        ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
-//
-//        List<ConfigEntity> configEntity = configRepository.findAll();
-//        Map<String, Object> propertySource = configEntity.get(0).getConfigInfo();
-//        configurableEnvironment.getPropertySources().addFirst(new MapPropertySource("app-config", propertySource));
-//    }
-//
-//}
+package com.minhhai.learn.configurations.properties;
+
+import com.minhhai.learn.entities.ConfigEntity;
+import com.minhhai.learn.repository.ConfigRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component("configurationPropertySource")
+public class ConfigurationPropertySource implements EnvironmentAware {
+
+    @Autowired
+    private ConfigRepository configRepository;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) environment;
+
+        List<ConfigEntity> configEntity = configRepository.findAll();
+        Map<String, Object> propertySource = configEntity.get(0).getConfigInfo();
+        configurableEnvironment.getPropertySources().addFirst(new MapPropertySource("app-config", propertySource));
+    }
+
+}
